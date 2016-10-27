47150b4fc07e39cc7d3f85002f788ec69a19f79d
nrew225 <nrew225@gmail.com>
2016-10-26 21:58:19 -0700

    Travis only test additions on PR
    some fixes in test.js

--- test.js
+++ test.js
@@ -1,105 +1,132 @@
-const fs = require('fs');
-var testStatus;
-var log = "{\n";
-var issuelog = '"message": "Line | Entry\\n----|----------------------\\n';
+const fs = require('fs')
+var log = '{\n'
+var issuelog = '"message": "Title | Entry\\n----|----------------------\\n'
 
-var file = fs.readFileSync('README.md', 'utf8')
-entryErrorCheck(file);
-return testStatus;
+var file = fs.readFileSync(process.argv[2], 'utf8')
+entryErrorCheck(file)
 
-function entryErrorCheck(md) {
-    var namepatt = /\*\s\[(.*?)\]/;
-    var entries = split(md);
-    var totalFail = 0;
-    var totalPass = 0;
-    var total = 0;
-    var entryArray = [];
-    for (var i = 0, len = entries.length; i < len; i++) {
-        entryArray[i] = new Object;
-        entryArray[i].raw = entries[i];
-        //console.log(entries[i]);
-        //console.log(entryArray[i].raw);
-        //console.log(entryFilter(entries[i]));
-        if (entryFilter(entries[i]) === true){
-            total += 1;
-            entryArray[i].name = namepatt.exec(entries[i])[1];
-            //console.log('name' + entryArray[i].name);
-            //entryArray[i].error = "";
-            entryArray[i].pass = findPattern(entries[i], i);
-            if (entryArray[i].pass == true) {
-                totalPass += 1;
-                //console.log(i + 1 + ". Pass: " + entryArray[i].name);
-            } else {    
-                console.log("Line #" + (i + 1) + ". Fail: " + entryArray[i].name + "\\n");
-                //console.log(entries[i]);
-                totalFail += 1;
-                issuelog += (i + 1) + " | " + entryArray[i].name;
-            }
-        }
+function entryErrorCheck (md) {
+  var namepatt = /\*\s\[(.*?)\]/
+  var entries = split(md)
+  var totalFail = 0
+  var totalPass = 0
+  var total = 0
+  var entryArray = []
+  for (var i = 0, len = entries.length; i < len; i++) {
+    entryArray[i] = new Object;
+    entryArray[i].raw = entries[i]
+    if (entryFilter(entries[i]) === true) {
+      total += 1
+      entryArray[i].name = namepatt.exec(entries[i])[1]
+      entryArray[i].pass = findPattern(entries[i])
+      if (entryArray[i].pass === true) {
+        totalPass += 1
+      } else {
+        console.log(entryArray[i].name + 'Failed.')
+        // entryArray[i].error = findError(entries[i]) //WIP
+        totalFail += 1
+        issuelog += entryArray[i].name + ' | ' + entries[i] + '\\n'
+      }
     }
-            if (totalFail > 0) {
-                console.log(totalFail + " Failed, " + totalPass + " Passed, of " + total);
-                log += '"error": "true",\n"title": "Found ' + totalFail + ' syntax error(s).",\n';
-                fs.writeFileSync('syntaxcheck.json', log + issuelog + '"\n}');
-                process.exit(1);
-            } else {
-                console.log(totalFail + " Failed, " + totalPass + " Passed, of " + total + "\n");
-                log += '"error": false,\n"title": "No errors found!"\n}';
-                fs.writeFileSync('syntaxcheck.json', log);
-                process.exit(0);      
-            }
-        //console.log(entries[i])
-        //console.log(totalPass + "|" + totalFail);
-    
+  }
+  if (totalFail > 0) {
+    console.log(totalFail + ' Failed, ' + totalPass + ' Passed, of ' + total)
+    log += '"error": "true",\n"title": "Found ' + totalFail + ' syntax error(s).",\n'
+    fs.writeFileSync('syntaxcheck.json', log + issuelog + '"\n}')
+    process.exit(1)
+  } else {
+    console.log(totalFail + ' Failed, ' + totalPass + ' Passed, of ' + total + '\n')
+    log += '"error": false,\n"title": "No errors found!"\n}'
+    fs.writeFileSync('syntaxcheck.json', log)
+    process.exit(0)
+  }
 };
 
-function entryFilter(md) {
-    var linepatt = /^\s{0,4}\*\s\[.*\`/;
-    return linepatt.test(md);
+function entryFilter (md) {
+  var linepatt = /^\s{0,4}\*\s\[.*`/
+  return linepatt.test(md)
 }
 
-function split(text){
-        //cutOne = text.split("<!-- BEGIN SOFTWARE LIST -->");
-        //cutTwo = cutOne[1].split("<!-- END SOFTWARE LIST -->");
-        //cutThree = cutTwo[0].replace(/^\#.*$|^\_.*$|^See.*$|^Some.*$|^\*.*\*$|^\s\*.*\*$|^CMS.*$|^\*\*\[.*$/mg, "");
-        return text.split(/\r?\n/);
+function split (text) {
+  return text.split(/\r?\n/)
 }
 
-function parseMD(md) {
-    return split(md).filter(entryFilter);
-    //console.log(entries);
-    //console.log (entries.length);
-    //console.log(entries[0]);
-    //console.log(entries[entries.length - 1]);
-}
+// function parseMD(md) {
+  // return split(md).filter(entryFilter);
+// }
 
-function findPattern(text, i) {
-    var nodnospatt = /\s{0,3}\*\s\[(.*?)\]\((.*?)\)\s\-\s(.{0,249}?\.\s)\`(.*?)\`\s\`(.*?)\`/; //Regex for entries with no demo and no source code
-    var slpatt = /\s{0,3}\*\s\[(.*?)\]\((.*?)\)\s\-\s(.{0,249}?\.\s)\(\[Demo\b\]\((.*?)\)\,\s\[Source Code\b\]\((.*?)\)\)\s\`(.*?)\`\s\`(.*?)\`/; //Regex for entries with demo and source code
-    var nodpatt = /\s{0,3}\*\s\[(.*?)\]\((.*?)\)\s\-\s(.{0,249}?\.\s)\(\[Source Code\]\((.*?)\)\)\s\`(.*?)\`\s\`(.*?)\`/; //Regex for entries with no demo
-    var nospatt = /\s{0,3}\*\s\[(.*?)\]\((.*?)\)\s\-\s(.{0,249}?\.\s)\(\[Demo\]\((.*?)\)\)\s\`(.*?)\`\s\`(.*?)\`/; //Regex for entries with no source code
-    var pnodnospatt = /\s{0,3}\*\s\[(.*?)\]\((.*?)\)\s\`(\⚠)\`\s\-\s(.{0,249}?\.\s)\`(.*?)\`\s\`(.*?)\`/; //Regex for entries with proprietary with no demo and no source code
-    var pslpatt = /\s{0,3}\*\s\[(.*?)\]\((.*?)\)\s\`(\⚠)\`\s\-\s(.{0,249}?\.\s)\(\[Demo\b\]\((.*?)\)\,\s\[Source Code\b\]\((.*?)\)\)\s\`(.*?)\`\s\`(.*?)\`/; //Regex for entries with proprietary with demo and source code
-    var pnodpatt = /\s{0,3}\*\s\[(.*?)\]\((.*?)\)\s\`(\⚠)\`\s\-\s(.{0,249}?\.\s)\(\[Source Code\]\((.*?)\)\)\s\`(.*?)\`\s\`(.*?)\`/; //Regex for entries with proprietary with no demo
-    var pnospatt = /\s{0,3}\*\s\[(.*?)\]\((.*?)\)\s\`(\⚠)\`\s\-\s(.{0,249}?\.\s)\(\[Demo\]\((.*?)\)\)\s\`(.*?)\`\s\`(.*?)\`/; //Regex for entries with proprietary with no source code
-    if (nodnospatt.test(text) == true) {
-        return true;
-    } else if (slpatt.test(text) == true) {
-        return true;
-    } else if (nodpatt.test(text) == true) {
-        return true;
-    } else if (nospatt.test(text) == true) {
-        return true;
-    } else if (pnodnospatt.test(text) == true) {
-        return true;
-    } else if (pslpatt.test(text) == true) {
-        return true;
-    } else if (pnodpatt.test(text) == true) {
-        return true;
-    } else if (pnospatt.test(text) == true) {
-        return true;
-    } else {
-        //document.getElementById("alert").innerHTML = "Error(s) Found, check your syntax!";
-        return false;
-    }
+function findPattern (text) {
+  var nodnospatt = /\s{0,4}\*\s\[(.*?)\]\((.*?)\)\s\-\s(.{0,249}?\.\s)`(.*?)`\s`(.*?)`/ // Regex for entries with no demo and no source code
+  var slpatt = /\s{0,4}\*\s\[(.*?)\]\((.*?)\)\s\-\s(.{0,249}?\.\s)\(\[Demo\b\]\((.*?)\),\s\[Source Code\b\]\((.*?)\)\)\s`(.*?)`\s`(.*?)`/ // Regex for entries with demo and source code
+  var nodpatt = /\s{0,4}\*\s\[(.*?)\]\((.*?)\)\s\-\s(.{0,249}?\.\s)\(\[Source Code\]\((.*?)\)\)\s`(.*?)`\s`(.*?)`/ // Regex for entries with no demo
+  var nospatt = /\s{0,4}\*\s\[(.*?)\]\((.*?)\)\s\-\s(.{0,249}?\.\s)\(\[Demo\]\((.*?)\)\)\s`(.*?)`\s`(.*?)`/ // Regex for entries with no source code
+  var pnodnospatt = /\s{0,4}\*\s\[(.*?)\]\((.*?)\)\s`(⚠)`\s\-\s(.{0,249}?\.\s)`(.*?)`\s`(.*?)`/ // Regex for entries with proprietary with no demo and no source code
+  var pslpatt = /\s{0,4}\*\s\[(.*?)\]\((.*?)\)\s`(⚠)`\s\-\s(.{0,249}?\.\s)\(\[Demo\b\]\((.*?)\),\s\[Source Code\b\]\((.*?)\)\)\s`(.*?)`\s`(.*?)`/ // Regex for entries with proprietary with demo and source code
+  var pnodpatt = /\s{0,4}\*\s\[(.*?)\]\((.*?)\)\s`(⚠)`\s\-\s(.{0,249}?\.\s)\(\[Source Code\]\((.*?)\)\)\s`(.*?)`\s`(.*?)`/ // Regex for entries with proprietary with no demo
+  var pnospatt = /\s{0,4}\*\s\[(.*?)\]\((.*?)\)\s`(⚠)`\s\-\s(.{0,249}?\.\s)\(\[Demo\]\((.*?)\)\)\s`(.*?)`\s`(.*?)`/ // Regex for entries with proprietary with no source code
+  if (nodnospatt.test(text) === true) {
+    return true
+  } else if (slpatt.test(text) === true) {
+    return true
+  } else if (nodpatt.test(text) === true) {
+    return true
+  } else if (nospatt.test(text) === true) {
+    return true
+  } else if (pnodnospatt.test(text) === true) {
+    return true
+  } else if (pslpatt.test(text) === true) {
+    return true
+  } else if (pnodpatt.test(text) === true) {
+    return true
+  } else if (pnospatt.test(text) === true) {
+    return true
+  } else {
+    return false
+  }
 };
+// WIP
+function findError (entry) {
+  var fixedEntry = entry
+  if (/\s{0,4}\*\s\[.*?\]\(http.*?\)/.test(entry) === false) { // Fix * site name/URL
+    console.log('marker2')
+    entry.replace(/^(\s*)\**\s*\[*([^()\[\]]*?)\]*\s*\(*(https*:\/\/[^ )]{1,})(\)*\s*`*⚠`*\s*-*\s*|\)*\s*-*\s*)(.*)/gi, function (match, p1, p2, p3, p4, p5) {
+      console.log('Error in [Name](URL)')
+      if (/⚠/.test(entry) === true) {
+        fixedEntry = p1 + '* [' + p2 + '](' + p3 + ') `⚠` - ' + p5
+      } else {
+        fixedEntry = p1 + '* [' + p2 + '](' + p3 + ') - ' + p5
+      }
+      console.log('Fixed: ' + fixedEntry)
+    })
+  }
+  if (/\s-\s/.test(entry) === false) {
+    console.log('marker3')
+    entry.replace(/(\s{0,3}\*\s\[.*?\]\(.*?\))(\s*`*⚠`*\s*-*\s*|\s*`*⚠`*\s*-*|`*⚠`*\s*-*\s*|`*⚠`*-*|\s*|\s*-*\s*|\s*-*|-*\s*|-*|\s*)(.{0,249}?\.)(.*)/gi, function (match, p1, p2, p3, p4) { // Fix space - space between link and descrition
+      console.log('Missing ` - ` between Site link and description.')
+      if (/⚠/.test(entry) === true) {
+        fixedEntry = p1 + ' `⚠` - ' + p3 + p4
+      } else {
+        fixedEntry = p1 + ' - ' + p3 + p4
+      }
+      console.log('Fixed: ' + fixedEntry)
+    })
+  }
+  if (/^\s{0,4}\*\s\[.*?\]\(.*?\).*?(\s\.|\)\)\s)`.*?`\s`.*?`/.test(entry) === false) {
+    console.log('marker4')
+    entry.replace(/^(\s*\*\s.*?\)\)|.*?\s\-\s.*?\.)(\s*`*\s*)(.*?)(\s*`\s*`\s*|\s*`\s*`*\s*|\s*`*\s*`\s*|\s)(.*?)`/gi, function (match, p1, p2, p3, p4, p5) { // Fix License and Language
+      console.log('Issue in Language or License')
+      fixedEntry = p1 + ' `' + p3 + '` `' + p5 + '`'
+      console.log('Fixed: ' + fixedEntry)
+    })
+  } else if (/demo/i.test(entry) === true) {
+
+  } else if (/source/i.test(entry) === true) {
+
+  }
+  console.log(findPattern(fixedEntry))
+  // if (findPattern(fixedEntry) === false) {
+    // findError(fixedEntry) // If it's not fixed let's try again.
+  // }
+}
+
+// else if (/source/i.test(entry) === true && /demo/i.test(entry) === true) {
